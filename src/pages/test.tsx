d50b2a48f733966ebc03931519f9ca234dedce98
Yuki Mizuno <muscular.mizuno@gmail.com>
2023-04-27 10:01:04 +0900

    ✨ constアサーション、ユニオン型の例を確認

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -13,13 +13,25 @@ export default function Home() {
   // ok
   foo = { bar: 'aaa', baz: 'ieeeeee' }
 
-  // numeric separator
+  // numeric separator 100,000,000のように読みやすく表現できる
   const number = 100_000_000
 
   typeof undefined
   // => "undefined"
   typeof null
   // => "object"
+
+  type Step = 'input' | 'confirm' | 'complete'
+
+  type ErrorCode = 400 | 401 | 402 | 403 | 404 | 405
+
+  const pikachu = {
+    name: 'pikachu',
+    no: 25,
+    genre: 'mouse pokémon',
+    height: 0.4,
+    weight: 6.0,
+  } as const
 
   return (
     <>
