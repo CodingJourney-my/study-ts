b25a45f76e776e099cb9417a9d4e915cfc5042a0
Yuki Mizuno <muscular.mizuno@gmail.com>
2023-05-05 12:26:38 +0900

    TypeScriptの型演習 2-Xを完了

--- notes/practice.ts
+++ notes/practice.ts
@@ -80,3 +80,102 @@ const res2 = myFilter(['foo', 'hoge', 'bar'], (str) => str.length >= 4)
 
 // エラー例
 // myFilter([1, 2, 3, 4, 5], (str) => str.length >= 4)
+
+// 2-2 ユニオン型
+type Speed = 'slow' | 'medium' | 'fast'
+
+function getSpeed(speed: Speed): number {
+  switch (speed) {
+    case 'slow':
+      return 10
+    case 'medium':
+      return 50
+    case 'fast':
+      return 200
+  }
+}
+
+// 使用例
+const slowSpeed = getSpeed('slow')
+const mediumSpeed = getSpeed('medium')
+const fastSpeed = getSpeed('fast')
+
+// エラー例
+// getSpeed('veryfast')
+
+// 2-3
+type OptionsObj = { capture?: boolean; once?: boolean; passive?: boolean }
+declare function addEventListener(
+  str: string,
+  func: () => void,
+  boolOrObj?: boolean | OptionsObj
+): void
+
+// 使用例
+addEventListener('foobar', () => {})
+addEventListener('event', () => {}, true)
+addEventListener('event2', () => {}, {})
+addEventListener('event3', () => {}, {
+  capture: true,
+  once: false,
+})
+
+// エラー例
+// addEventListener('foobar', () => {}, 'string')
+// addEventListener('hoge', () => {}, {
+//   capture: true,
+//   once: false,
+//   excess: true,
+// })
+
+// 2-4 インターセクション型
+const giveId = <T>(obj: T): T & { id: string } => {
+  const id = '本当はランダムがいいけどここではただの文字列'
+  return {
+    ...obj,
+    id,
+  }
+}
+
+// 使用例
+const obj1: {
+  id: string
+  foo: number
+} = giveId({ foo: 123 })
+const obj2: {
+  id: string
+  num: number
+  hoge: boolean
+} = giveId({
+  num: 0,
+  hoge: true,
+})
+
+// エラー例
+// const obj3: {
+//   id: string
+//   piyo: string
+// } = giveId({
+//   foo: 'bar',
+// })
+
+// 2-5 タプル型
+// 使用例
+// number型のステートを宣言 (numStateはnumber型)
+type UpdateArg<T> = T | ((oldState: T) => void)
+declare function useState<T>(state: T): [T, (arg: UpdateArg<T>) => void]
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [numState, setNumState] = useState(0)
+// setNumStateは新しい値で呼び出せる
+setNumState(3)
+// setNumStateは古いステートを新しいステートに変換する関数を渡すこともできる
+setNumState((state) => state + 10)
+
+// 型引数を明示することも可能
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [anotherState, setAnotherState] = useState<number | null>(null)
+setAnotherState(100)
+
+// エラー例
+// setNumState('foobar')
