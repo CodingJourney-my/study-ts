4058382d5d97875c63f6c959dad60cf4009f1ae6
Yuki Mizuno <muscular.mizuno@gmail.com>
2023-05-10 12:51:33 +0900

    TypeScriptの型演習 3-3~5まで

--- notes/practice.ts
+++ notes/practice.ts
@@ -209,7 +209,7 @@ Map {
 // エラー例
 // mapFromArray(data, 'age')
 
-// 3-2
+// 3-2 Partial
 type MyPartial<T> = { [K in keyof T]?: T[K] }
 // 使用例
 /*
@@ -227,3 +227,83 @@ type T2 = MyPartial<{
     piyo: number
   }
 }>
+
+// 3-3 まだクラスの扱いが理解できていないため省略
+
+// 3-4 reducer
+// 現在のレベルで思いついた回答 要件を満たせない
+// type ActionType = {
+//   type: 'increment' | 'decrement' | 'reset'
+//   amount?: number
+//   value?: number
+// }
+
+// オブジェクトごとパーシャルにすることでより正確な型設定ができる。
+type Action =
+  | {
+      type: 'increment'
+      amount: number
+    }
+  | {
+      type: 'decrement'
+      amount: number
+    }
+  | {
+      type: 'reset'
+      value: number
+    }
+
+const reducer = (state: number, action: Action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + action.amount
+    case 'decrement':
+      return state - action.amount
+    case 'reset':
+      return action.value
+  }
+}
+
+// 使用例
+reducer(100, {
+  type: 'increment',
+  amount: 10,
+}) === 110
+reducer(100, {
+  type: 'decrement',
+  amount: 55,
+}) === 45
+reducer(500, {
+  type: 'reset',
+  value: 0,
+}) === 0
+
+// エラー例
+// reducer(0, {
+//   type: 'increment',
+//   value: 100,
+// })
+
+// 3-5 undefinedな引数
+// type Func<A, R> = (arg: A) => R
+// f2のようにAがundefined型のときは引数無しで呼べる
+// v3のように明示的にundefinedを渡して呼び出すのもOK
+// v4のように、引数がundefined以外のときは引数の省略は許しません
+
+// undefined extends Aで、A内にundefinedが含まれるかどうかを確認できる
+type Func<A, R> = undefined extends A ? (arg?: A) => R : (arg: A) => R
+
+// 使用例
+const f1: Func<number, number> = (num) => num + 10
+const v1: number = f1(10)
+
+const f2: Func<undefined, number> = () => 0
+const v2: number = f2()
+const v3: number = f2(undefined)
+
+const f3: Func<number | undefined, number> = (num) => (num || 0) + 10
+const v4: number = f3(123)
+const v5: number = f3()
+
+// エラー例
+// const v6: number = f1()
